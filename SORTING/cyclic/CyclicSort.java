a17478ccbd3de830b980380c52a80f977d1d893a
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-25 17:38:02 +0530

    learn about syclic sort

---
+++ SORTING/cyclic/CyclicSort.java
@@ -0,0 +1,35 @@
+package SORTING.cyclic;
+
+import java.util.Arrays;
+
+/**
+ * CyclicSort
+ */
+public class CyclicSort {
+
+    public static void main(String[] args) {
+        // unsort arraycd
+        int[] nums = { 2, 0, 1, 3 };
+        cyclicSortFN(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    // n = 4; [4,0,1,3] [1,2,3,4]
+
+    static void cyclicSortFN(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == i) {
+                i++;
+            } else {
+                swap(nums, nums[i], i);
+            }
+        }
+    }
+
+    static void swap(int[] nums, int i, int correct) {
+        int temp = nums[i];
+        nums[i] = nums[correct];
+        nums[correct] = temp;
+    }
+}
