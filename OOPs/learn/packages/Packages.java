c4443693b375f8b4e4b539437f9573ef74d3aee8
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-10 22:40:00 +0530

    started to learn about packages

---
+++ OOPs/learn/packages/Packages.java
@@ -0,0 +1,17 @@
+package oops.learn.packages;
+
+/**
+ * @author abhay
+ * Packages
+ */
+public class Packages {
+    public static void main(String[] args) {
+        System.out.println("Packages");
+    }
+    
+}
+// packages are just folders
+// to import a package, we need to use import keyword;
+// if import is not globally available, we need to use static import
+// import static __package__.__class__.__method__;
+// how to import not global methods ?
---
+++ OOPs/learn/packages/b/Greet.java
@@ -0,0 +1,16 @@
+package oops.learn.packages.b;
+
+/**
+ * Greet
+ */
+public class Greet {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+
+    // make and export a method
+    public static void message() {
+        System.out.println("Hello World from oops.learn.packages.b.");
+    }
+}
