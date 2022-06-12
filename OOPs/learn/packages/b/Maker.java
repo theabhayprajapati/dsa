13177d7c62c5768cfb0b293b8c924cf4251f8ae2
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-12 20:25:52 +0530

    make file

---
+++ OOPs/learn/packages/b/Maker.java
@@ -0,0 +1,12 @@
+package oops.learn.packages.a;
+public class Maker  {
+    public static void main(String[] args) {
+        message();
+        helloWorld();
+    }
+    public static void message(){
+        System.out.println("this is osm");
+//        System.out.println(Maker);
+        helloWorld();
+    }
+}
