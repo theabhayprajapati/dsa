b58573f9ade0d8e7299cc5dcc0738d7d11335623
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-03 11:50:14 +0530

    enter

--- Recursion/tail/git.sh
+++ Recursion/tail/git.sh
@@ -5,5 +5,9 @@ git commit -m "$1"
 git push 
 # msg green color msg
 echo -e "\e[32m"
-echo "Commit and push done $"
+echo "Commit and push done "
 echo -e "\e[0m"
+# in blue
+echo -e "\e[34m"
+echo "Now you can go to github.com and see your changes"
+
