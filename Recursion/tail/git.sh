beaa66a585e98599ab6f2e430e1d95c27f9c4a74
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-03 11:53:18 +0530

    added

--- Recursion/tail/git.sh
+++ Recursion/tail/git.sh
@@ -15,6 +15,10 @@ echo "Commit and push done :"
 echo -e "\e[0m"
 # in blue
 echo -e "\e[34m"
-echo "Now you can go to github.com and see your changes"
+echo ""
+# same line different color
+echo "git status"
+echo ""
+
 echo -e "\e[0m"
 
